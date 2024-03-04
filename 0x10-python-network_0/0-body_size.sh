e37de572bba177bea0d8c40b50b311101b9fdaa5
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-04 15:27:06 +0000

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays the size of the body of the response
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 [URL]"
-else
-    SIZE=$(curl -sI "$1" | grep -i Content-Length | awk {print $2})
-    echo $SIZE
-fi 
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
