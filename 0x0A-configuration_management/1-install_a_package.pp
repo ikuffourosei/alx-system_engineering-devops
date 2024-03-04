5d5da67957f9d2f31dd707691c286959ba987b6f
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-04 16:18:01 +0000

    puppet config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# puppet script that install flask, version 2.1.0
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+package { 'Werkzeug':
+  ensure   => '2.2',
+  provider => 'pip3',
+}
