5d6c4c6431d7e189b9350f59e95288efb9ce14c1
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-27 23:29:23 +0000

    write to a cvs file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format.
+-   run by ./script <user_id>
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(f"{url}users", params={"id": argv[1]}).json()
+    todo = requests.get(f"{url}todos", params={"userId": argv[1]}).json()
+    filename = '{}'.format(argv[1]) + '.csv'
+    with open('{}'.format(filename), 'w', newline='') as file:
+        writer = csv.writer(file)
+        for data in todo:
+            writer.writerow(["{}".format(argv[1]),
+                             "{}".format(user[0].get('name')),
+                             "{}".format(data['completed']),
+                             "{}".format(data['tite'])])
