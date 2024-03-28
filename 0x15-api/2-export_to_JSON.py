00a62a2dd4beaefeef43205bb4974ee72ebd422b
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-28 02:09:53 +0000

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(f"{url}users/{argv[1]}").json()
+    todo = requests.get(f"{url}todos", params={"userId": argv[1]}).json()
+    username = user['username']
+    val = []
+    for items in todo:
+        data = {}
+        data['task'] = items['title']
+        data['completed'] = items['completed']
+        data['username'] = username
+        val.append(data)
+
+    final_data = {"{}".format(argv[1]): val}
+    filename = '{}'.format(argv[1]) + '.json'
+    with open('{}'.format(filename), 'w', encoding='utf-8') as file:
+        json.dump(final_data, file)
